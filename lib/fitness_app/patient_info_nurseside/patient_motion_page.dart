60c6a37bb3a869c049760e55fe414901b0d3ba44
iamgeorge <wangxiiq200@icloud.com>
2024-08-05 20:13:17 -0400

    fix motion bug

--- lib/fitness_app/patient_info_nurseside/patient_motion_page.dart
+++ lib/fitness_app/patient_info_nurseside/patient_motion_page.dart
@@ -4,71 +4,50 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class PatientMotionPage extends StatefulWidget {
-  const PatientMotionPage({Key? key, required this.patientID}) : super(key: key);
   final String patientID;
+
+  const PatientMotionPage({Key? key, required this.patientID})
+      : super(key: key);
 
   @override
   State<PatientMotionPage> createState() => _PatientMotionPageState();
 }
 
-class _PatientMotionPageState extends State<PatientMotionPage> with TickerProviderStateMixin {
-  late List<Map<String, dynamic>> _statusMessages = [];
-  AnimationController? animationController;
+class _PatientMotionPageState extends State<PatientMotionPage> {
+  late List<Map<String, dynamic>> _statusMessages;
 
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-        duration: const Duration(milliseconds: 800), vsync: this);
-    animationController!.forward();
-    _InitConfig();
-  }
-
-  void _InitConfig() async {
-    setState(() {
-      _fetchPosts();
-    });
+    _statusMessages = [];
+    _fetchPosts();
   }
 
   Future<void> _fetchPosts() async {
-    var url = Uri.parse('http://43.136.14.179:5001/motion/fetch');
+    var url = Uri.parse('http://10.0.2.2:5001/motion/fetch');
     try {
       var response = await http.post(
         url,
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode(<String, dynamic>{'username': widget.patientID}),
+        headers: {'Content-Type': 'application/json; charset=UTF-8'},
+        body: jsonEncode({'username': widget.patientID}),
       );
-
       if (response.statusCode == 200) {
         List<dynamic> responseData = jsonDecode(response.body);
-        _statusMessages = responseData.map((post) {
-          return {
-            'content': post['content'],
-            'time': post['time'],
-            'post_id': post['post_id']
-          };
-        }).toList();
-        setState(() {});
+        setState(() {
+          _statusMessages = responseData
+              .map((post) => {
+                    'content': post['content'],
+                    'time': post['time'],
+                    'post_id': post['post_id']
+                  })
+              .toList();
+        });
       } else {
         print('Request failed with status: ${response.statusCode}.');
-        setState(() {
-          _statusMessages = [];
-        });
       }
     } catch (e) {
       print('Error fetching posts: $e');
-      setState(() {
-        _statusMessages = [];
-      });
     }
-  }
-
-  @override
-  void dispose() {
-    animationController?.dispose();
-    super.dispose();
   }
 
   @override
@@ -90,66 +69,77 @@ class _PatientMotionPageState extends State<PatientMotionPage> with TickerProvid
               delegate: SliverChildBuilderDelegate(
                 (context, index) {
                   final message = _statusMessages[index];
-                  return Dismissible(
-                    key: Key(message['post_id'].toString()),
-                    direction: DismissDirection.endToStart,
-                    background: Container(
-                      color: Colors.red,
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      alignment: Alignment.centerRight,
-                      child: const Icon(Icons.delete, color: Colors.white),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: CupertinoColors.systemGrey6,
-                            borderRadius: BorderRadius.circular(12),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.1),
-                                blurRadius: 10,
-                                offset: Offset(0, 2),
-                              )
-                            ]),
-                        child: ListTile(
-                          contentPadding: const EdgeInsets.all(16),
-                          title: Text(
-                            message['content'],
-                            style: const TextStyle(fontSize: 16),
-                            maxLines: 3,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          subtitle: Text(
-                            message['time'],
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                          onTap: () {
-                            showCupertinoModalPopup(
-                              context: context,
-                              builder: (context) {
-                                return CupertinoAlertDialog(
-                                  content: Text(message['content']),
-                                  actions: [
-                                    CupertinoDialogAction(
-                                      child: const Text('关闭'),
-                                      onPressed: () =>
-                                          Navigator.of(context).pop(),
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                  );
+                  return _buildListTile(context, message);
                 },
                 childCount: _statusMessages.length,
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildListTile(BuildContext context, Map<String, dynamic> message) {
+    return Dismissible(
+      key: Key(message['post_id'].toString()),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Colors.red,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        alignment: Alignment.centerRight,
+        child: const Icon(Icons.delete, color: Colors.white),
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _statusMessages.removeWhere(
+              (element) => element['post_id'] == message['post_id']);
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Material(
+          // Ensure ListTile has a Material ancestor
+          child: Container(
+            decoration: BoxDecoration(
+              color: CupertinoColors.systemGrey6,
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 10,
+                  offset: const Offset(0, 2),
+                )
+              ],
+            ),
+            child: ListTile(
+              contentPadding: const EdgeInsets.all(16),
+              title: Text(
+                message['content'],
+                style: const TextStyle(fontSize: 16),
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
+              subtitle: Text(
+                message['time'],
+                style: const TextStyle(color: Colors.grey),
+              ),
+              onTap: () {
+                showCupertinoModalPopup(
+                  context: context,
+                  builder: (context) => CupertinoAlertDialog(
+                    content: Text(message['content']),
+                    actions: [
+                      CupertinoDialogAction(
+                        child: const Text('关闭'),
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
         ),
       ),
     );
