9fab896231baf7f47008f0d6e283b1cf21e8168d
wek_deng <939681959@qq.com>
2024-09-01 21:31:44 +0800

    修改服务器ip设置

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
 class Config {
   static const bool isProduction = false;
 
-  static const String productionBaseUrl = 'http://43.136.14.179:5001';
+  static const String productionBaseUrl = 'http://43.136.52.103:5001';
   static const String developmentBaseUrl = 'http://10.0.2.2:5001';
 
   static String get baseUrl {
